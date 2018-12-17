8e738f9e01cb5f65279b64363ee525e90faa6d53
Palina Pasiukevich <palina.pasiukevich@gmail.com>
2018-12-17 14:06:49 -0500

    added firebase to the project and changed the colors

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -13,12 +13,3 @@ Vue.use(Vuetify, {
 })
 // overriden default colors
 
-
-// green - #6ca78c;
-//yellow - #ECB866;
-// red -  #e25021;
-
-//blue - #7495B1  - #85a7c3   bege - #dedad4 - btn -#BDD8D4
-// purple - #9c88b5 // black - #2F2F2F              --blue - #7299b1
-
-//button - #DFD2C3                   ---#96b19c
